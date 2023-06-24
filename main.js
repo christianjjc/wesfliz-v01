d009edc7e1db2cc4582b77deab48c9a695959c45
christianjjc <christianjjc@gmail.com>
2023-06-23 23:12:13 -0500

    inicial

---
+++ main.js
@@ -0,0 +1,15 @@
+/* (async () => {
+  await fetch("https://www.omdbapi.com/?apikey=24eff419&s=movie&r=json&page=1")
+  //await fetch("peliculas.json")
+    .then((res) => res.json())
+    .then((res) => {
+      peliculas = res;
+    });
+  console.log(peliculas.Search);
+})(); */
+
+const contenedor = document.getElementById("listadoPeliculas");
+
+const listarPeliculas = (array) => {
+  array.map((el, index) => {});
+};
