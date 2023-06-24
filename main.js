9dfdf8387210bbc7f8f9e8315bcd8703134f5d7f
christianjjc <christianjjc@gmail.com>
2023-06-24 02:32:44 -0500

    terminado el listado en desktop y mobile - tambien hace click

--- main.js
+++ main.js
@@ -8,15 +8,33 @@
   console.log(peliculas.Search);
 })(); */
 
+const muestraPeliculas = async (maxPfila, idContenedor, clasDiv) => {
+  let cantFilas = Math.ceil(peliculas.length / maxPfila);
+  let html = "";
+  for (let i = 1; i <= cantFilas; i++) {
+    html += `<div class="${clasDiv}">`;
+    imgs = peliculas.slice((i - 1) * maxPfila, i * maxPfila);
+    imgs.map((el) => {
+      html += `<img id="${el.imdbID}" class="imgpelicula" src="${el.Poster}" alt="${el.Title}">`;
+    });
+    html += `</div>`;
+  }
+  document.getElementById(idContenedor).innerHTML = html;
+};
+
 /* Agregamos eventos a las imagenes de cada encuentro */
-const eventoImg = (() => {
-  let imgBtn = document.getElementsByClassName(`imgpelicula`);
+const eventoImg = async () => {
+  let imgBtn = document.getElementsByClassName("imgpelicula");
   for (let i = 0; i < imgBtn.length; i++) {
     imgBtn[i].addEventListener("click", () => {
       const alt = imgBtn[i].alt;
       alert(alt);
     });
   }
-})();
+};
 
-const oo = [];
+(async () => {
+  await muestraPeliculas(6, "container-desktop", "lista");
+  await muestraPeliculas(2, "container-mobile", "lista-mobile");
+  await eventoImg();
+})();
