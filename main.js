08709cabc71b68122fb69b20b30cf5946a965634
christianjjc <christianjjc@gmail.com>
2023-06-24 21:31:51 -0500

    modal aggregado y funcionanado. enlacea a imdb y busqueda a youtube (por enlace a web)

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const peliculas = [];
+
 const getPeliculas = async (valor = "") => {
   let result = [];
   await fetch(
@@ -20,23 +22,13 @@ const muestraPeliculas = async (array, maxPfila, idContenedor, clasDiv) => {
       html += `<div class="${clasDiv}">`;
       const imgs = array.slice((i - 1) * maxPfila, i * maxPfila);
       imgs.map((el) => {
-        html += `<img id="${el.imdbID}" class="imgpelicula" src="${el.Poster}" alt="${el.Title}">`;
+        html += `<img id="${el.imdbID}" class="imgpelicula" src="${el.Poster}" alt="${el.Title}" data-bs-toggle="modal" data-bs-target="#modalDetalle">`;
       });
       html += `</div>`;
     }
     document.getElementById(idContenedor).innerHTML = html;
   } catch (error) {
     console.error(error);
-  }
-};
-
-const eventoImg = () => {
-  let imgBtn = document.getElementsByClassName("imgpelicula");
-  for (let i = 0; i < imgBtn.length; i++) {
-    imgBtn[i].addEventListener("click", () => {
-      const alt = imgBtn[i].alt;
-      alert(alt);
-    });
   }
 };
 
@@ -54,6 +46,39 @@ window.addEventListener("mouseout", () => {
   }
 });
 
+const muestraDetalle = (i, array) => {
+  const modal = document.getElementById("staticBackdropLabel");
+  modal.innerText = array[i].Title;
+  const imgContainer = document.getElementById("imgContainer");
+  imgContainer.innerHTML = `<img id="${array[i].imdbID}" src="${array[i].Poster}" alt="${array[i].Title}" class="imgDetalle">`;
+  const link = `https://www.imdb.com/title/${array[i].imdbID}/`;
+  let html = ``;
+  html += `<p><span class="fs-6">Id:</span> ${array[i].imdbID}</p>`;
+  html += `<p><span class="fs-6">Año:</span> ${array[i].Year}</p>`;
+  html += `<p><span class="fs-6">Tipo:</span> ${array[i].Type}</p>`;
+  html += `<div class="d-grid gap-3  col-6 mx-auto">`;
+  html += `<a class="btn btn-warning" href="${link}" target="_blank">Ver detalles en iMDB</a>`;
+  html += `<a class="btn btn-warning" href="https://www.youtube.com/results?search_query=${array[i].Title}+trailer" target="_blank">Buscar trailer en Youtube</a>`;
+  html += `</div>`;
+  document.getElementById("dataContainer").innerHTML = html;
+};
+
+const obtenerDetalle = async (id) => {
+  const result = await JSON.parse(localStorage.getItem("peliculas"));
+  const index = await result.findIndex((e) => e.imdbID == id);
+  muestraDetalle(index, result);
+};
+
+const eventoImg = (fn) => {
+  let imgBtn = document.getElementsByClassName("imgpelicula");
+  for (let i = 0; i < imgBtn.length; i++) {
+    imgBtn[i].addEventListener("click", () => {
+      const id = imgBtn[i].id;
+      fn(id);
+    });
+  }
+};
+
 async function cargaDatos(inicio = true, valor = "") {
   let vp = 0;
   try {
@@ -65,8 +90,10 @@ async function cargaDatos(inicio = true, valor = "") {
       vp = 6;
     }
     const peliculas = await getPeliculas(!inicio ? valor : "");
+    localStorage.clear;
+    localStorage.setItem("peliculas", JSON.stringify(peliculas));
     await muestraPeliculas(peliculas, vp, "container-desktop", "lista");
-    eventoImg();
+    eventoImg(obtenerDetalle);
   } catch (error) {
     console.error(error);
   }
